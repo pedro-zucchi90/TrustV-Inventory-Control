52c7e1cde0590799e3584f9ac02785cb9bb91569
Pedro Zucchi <predozucchi@gmail.com>
2025-08-31 17:17:50 -0300

    git pullo commito -  merge conflicto

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-
-
-
-from flask import Flask, render_template, redirect, url_for, flash, request, Response, jsonify, make_response
+from flask import Flask, render_template, redirect, url_for, flash, request, Response, jsonify, make_response, abort
 import csv
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Produto
